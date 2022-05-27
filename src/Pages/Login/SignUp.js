c33674ee0091fe7292b4bcd6d468461d071e35b9
saifur550 <srstap550@gmail.com>
2022-05-27 09:31:03 +0600

    jwt tooken setup

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/UseToken';
 
 const SignUp = () => {
     const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
@@ -16,6 +17,7 @@ const SignUp = () => {
     ] = useCreateUserWithEmailAndPassword(auth);
 
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [token]  = useToken(user || user1);
 
     const navigate = useNavigate();
 
@@ -28,16 +30,15 @@ const SignUp = () => {
     if (error || error1 || updateError) {
         signInError = <p className='text-red-500'><small>{error?.message || error1?.message || updateError?.message}</small></p>
     }
-
-    if (user || user1) {
-        console.log(user || user1);
+    if (token) {
+        navigate('/purchase');
     }
 
     const onSubmit = async data => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log('user update');
-        navigate('/purchase');
+        // navigate('/purchase');
     }
 
     
